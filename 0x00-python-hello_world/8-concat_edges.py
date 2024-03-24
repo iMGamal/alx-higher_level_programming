1ba801543bb7abd8ddb3deb96da934483a63c3e5
iMGamal <mohamed.gamal126027@gmail.com>
2024-03-24 10:26:35 +0000

    120

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
          language that combines remarkable power with very clear syntax"
-str = str[39:54] + str.join(' ') + str[55:66] + str[115:119] + str[:6]
+str = str.split()[5] + str.join(' ') + str.split()[6] + str.join(' ') + str.split()[12] + str.join(' ') + str.split()[0]
 print(str)
