d9b9db3ac96453953f64e5329e5191390b9b1509
iMGamal <mohamed.gamal126027@gmail.com>
2024-03-25 19:12:00 +0000

    basic

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
          language that combines remarkable power with very clear syntax"
-str = str.split()[5] + str.join(' ') + str.split()[6] + str.join(' ') + str.split()[12] + str.join(' ') + str.split()[0]
+str = str[39:46] + str[46:54] + str[55:66] + str[115:119] + str[:6]
 print(str)
--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,2 +1,4 @@
 #!/usr/bin/python3
-f = open("/alx-higher_level_programming/0x00-python-hello_world/zed.py", "r")
+def zed.py
+import zed.py
+
