95fcd5e0962311c97bfbe49eb8b6537a75bb4ba4
iMGamal <mohamed.gamal126027@gmail.com>
2024-04-03 19:48:03 +0000

    read

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 # function that prints elements of a list
 def safe_print_list(my_list=[], x=0):
-    for x in my_list:
+    my_list=[n]
+    while x >= n:
         try:
             print(my_list'\n')
             break
         except: x = 0:
             print("invalid")
-
