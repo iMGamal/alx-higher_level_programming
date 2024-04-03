982f49da4c75a15fc11237edca44ad45a43c48be
iMGamal <mohamed.gamal126027@gmail.com>
2024-04-03 20:35:53 +0000

    test

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,9 @@ def safe_print_list(my_list=[], x=0):
     my_list=[n]
     while x >= n:
         try:
-            print(my_list)
+            print(n)
+        except:
+            x = 0
+            print("invalid input")
             break
-        except: x = 0
-        print("invalid")
+        return x
