420c0b3fdeefa900132693a62cb2f26444ed3c15
iMGamal <mohamed.gamal126027@gmail.com>
2024-04-04 14:19:36 +0000

    break

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number % 10 > 5:
-    print("Last digit of" + " " + str(number) + " " + "is" + " " + str(number % 10) + " " + "and is greater than 5")
-elif number % 10 == 0:
-    print("Last digit of" + " " + str(number) + " " + "is" + " " + str(number % 10) + " " + "and is 0")
-elif number % 10 < 6 and number % 10 != 0:
-    print("Last digit of" + " " + str(number) + " " + "is" + " " + str(number % 10) + " " + "and less than 6 and not 0")
+stampa = ("Last digit of" + " " + str(number) + " " + "is" + " ")
+if number > 0:
+    while number % 10 > 5:
+        print(stampa + str(number % 10) + " " + "and is greater than 5")
+        break
+    while number % 10 == 0:
+        print(stampa + str(number % 10) + " " + "and is 0")
+        break
+    while number % 10 < 6 and number % 10 != 0:
+        print(stampa + str(number % 10) + " " + "and is less than 6 and not 0")
+        break
+elif number < 0:
+    while number % -10 < 6 and number % 10 != 0:
+        print(stampa + str(number % -10) + " " + "and is less than 6 and not 0")
+        break
