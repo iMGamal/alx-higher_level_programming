31f537fca72f1f7febfab45159c95a33fabe4540
iMGamal <mohamed.gamal126027@gmail.com>
2024-04-17 18:22:59 +0000

    test6

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
+"""You are not allowed to import any modules."""
 
-# You are not allowed to import any modules.
 
 def read_file(filename=""):
-# Prototype for a function that reads files.
+"""Prototype for a function that reads files."""
     with open('""', 'r', encoding="utf-8") as f:
         data = f.readlines()
         print(data)
