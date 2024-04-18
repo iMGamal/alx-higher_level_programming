c6f6ce936cc9a325bda7cf5aae48c30b352dae07
iMGamal <mohamed.gamal126027@gmail.com>
2024-04-18 06:46:41 +0000

    test12

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -12,6 +12,6 @@ def read_file(filename=""):
     Returns:
     - str: the result of reading.
     """
-    with open('filename=""', 'r', encoding="utf-8") as f:
-        data = f.readlines()
+    with open(filename, encoding="utf-8") as f:
+        data = f.read()
         print(data)
