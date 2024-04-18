ec3753fc196950c9a5e498f07234c38d360e35c4
iMGamal <mohamed.gamal126027@gmail.com>
2024-04-18 06:27:40 +0000

    test9

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
+
 """
 defining read_file function.
 """
-
-
 def read_file(filename=""):
-"""
-reads filename with utf-8 encoding.
+    """
+    reads filename with utf-8 encoding.
 
-Returns:
+    Returns:
     - str: the result of reading.
-"""
+    """
     with open('""', 'r', encoding="utf-8") as f:
         data = f.readlines()
         print(data)
