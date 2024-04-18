0b2d572b2992b0e3896f9b15bbbf1398f27b3180
iMGamal <mohamed.gamal126027@gmail.com>
2024-04-18 07:10:53 +0000

    test1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+"""
+defining write_file function.
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    writes filename with utf-8 encoding.
+
+    Parameters:
+    - filename (str): the file with text to be overwritten.
+    - text (str): the file with new text to be written.
+
+    Return:
+    - int; the number of characters written.
+    """
+    print(f.write(), end="")
