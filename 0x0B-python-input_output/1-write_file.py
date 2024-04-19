c5bf032dd09b90f83e2c5bab474171a110ed784a
iMGamal <mohamed.gamal126027@gmail.com>
2024-04-19 16:28:55 +0000

    09

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -7,4 +7,8 @@ Defining a writing function.
 
 def write_file(filename="", text=""):
     """
+    sfhjkkkkl
+    """
+    with open(filename, encoding="utf-8") as f:
+        print(f.write(text))
 
