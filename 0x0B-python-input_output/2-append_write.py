ead61a357688fcd72f9b600044197848ebfbe9c7
iMGamal <mohamed.gamal126027@gmail.com>
2024-04-19 18:24:23 +0000

    3mon

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+defining function that appends to files.
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    Args:
+        filename (str): the string of text to beappended
+        text (str): the string to be appended
+    Return:
+        characters (int): the numbers of characters added
+    """
+    with open(filename, "a", encoding="utf-8") as f:
+        return(f.append(text))
