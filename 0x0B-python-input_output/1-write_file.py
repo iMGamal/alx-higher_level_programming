4e1aa03acae34cdfe3e90eb0cd34f27109118217
iMGamal <mohamed.gamal126027@gmail.com>
2024-04-19 12:49:42 +0000

    test12

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,16 +1,10 @@
 #!/usr/bin/python3
+
 """
 Defining a writing function.
 """
 
 
 def write_file(filename="", text=""):
-    """Function that writes a string with utf-8 encoding.
-    Args:
-        filename (str): the file passed to function.
-        text (str): the new string to be written.
-    Return:
-        The number of characters written.
     """
-    with open(filename, encoding="utf-8") as f:
-        print(f.write(text) + end="")
+
