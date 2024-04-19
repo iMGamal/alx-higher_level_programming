44db56711c2d43d667e6c78bcb0f8eac6d12c77d
iMGamal <mohamed.gamal126027@gmail.com>
2024-04-19 16:36:14 +0000

    010

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -7,8 +7,11 @@ Defining a writing function.
 
 def write_file(filename="", text=""):
     """
-    sfhjkkkkl
+    Args:
+        filename (str): the text to be overwritten.
+        text (str): the new text to be written.
+    Return:
+        characters (int): the number of characters printed.
     """
     with open(filename, encoding="utf-8") as f:
         print(f.write(text))
-
