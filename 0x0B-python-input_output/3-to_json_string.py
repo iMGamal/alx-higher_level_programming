2f38eae96c6c32efb529df366385739a471757f5
iMGamal <mohamed.gamal126027@gmail.com>
2024-04-19 22:29:14 +0000

    3mon

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,17 @@
+#!/bin/usr/python3
+
+"""
+defining a json string.
+"""
+
+
+def to_json_string(my_obj):
+    """
+    Args:
+        my_obj (str): a string to be converted to json style.
+    Return:
+        json (str): JSON represntation of a strong.
+    """
+    with open(my_obj, "w", encoding="utf-8") as f:
+        f = '{{"my_obj": "{}"}}'.format(my_obj)
+        return (f.write())
