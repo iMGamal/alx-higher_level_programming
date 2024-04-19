4551a16f8e85c1118d6e8a992ec8f82172707600
iMGamal <mohamed.gamal126027@gmail.com>
2024-04-19 16:41:19 +0000

    012

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -14,4 +14,4 @@ def write_file(filename="", text=""):
         characters (int): the number of characters printed.
     """
     with open(filename, encoding="utf-8") as f:
-        print(f.write(text))
+        return(f.write(text))
