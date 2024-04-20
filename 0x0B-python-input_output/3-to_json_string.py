a4b9921a8174af7375d59cbb54c7ab5845781e9d
iMGamal <mohamed.gamal126027@gmail.com>
2024-04-20 06:14:45 +0000

    10

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -10,6 +10,6 @@ def to_json_string(my_obj):
     Args:
         my_obj (str): a string to be converted to json style.
     Return:
-        json.dumps(my_obj) (str): JSON represntation of a strong.
-    """
+        json represntation of an object (string).
+        """
    return (json.dumps(my_obj))
