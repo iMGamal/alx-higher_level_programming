773f282be2b0d1b49ced3862a9fe5cbb4bce5a85
iMGamal <mohamed.gamal126027@gmail.com>
2024-04-20 06:22:49 +0000

    12

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -7,7 +7,6 @@ defining a json string.
 
 def to_json_string(my_obj):
     """
-    Return:
-        json represntation of an object (string).
-        """
+    returns json represntation of an object (string).
+    """
    return (json.dumps(my_obj))
