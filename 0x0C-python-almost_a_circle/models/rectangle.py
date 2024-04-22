87bf4599b542f92911cc2e653c727d0125886780
iMGamal <mohamed.gamal126027@gmail.com>
2024-04-22 04:39:28 +0000

    thunder

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+"""Module for class rectangle."""
+from base import Base
+
+
+class Rectangle(Base):
+    """class rectangle"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """class constructor"""
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """width of rectangle"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def height(self):
+        """height of rectangrl"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+    @property
+    def x(self):
+        """x of rectangle"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @property
+    def y(self):
+        """y of rectangle"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
+
+    def validate(self, name, value):
+        """Method for validating value"""
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be > 0".format(name))
+        elif name < 0:
+            raise ValueError("{} must be >= 0".format(name))
+
+    def area(self):
+        """Method to calculate rectangle area"""
+        return (self.width * self.height)
+
+    def display(self):
+        """Method to display rectangle"""
+        string = (self.width * '#' + '\n') * self.height
+        return(string)
+
+r1 = Rectangle(7, 11)
+print(r1.display())
