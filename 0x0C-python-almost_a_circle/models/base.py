bf9c6988cee5285923fa03d9faa104dfe6d8866f
iMGamal <mohamed.gamal126027@gmail.com>
2024-04-22 01:09:32 +0000

    1

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defining Module for class Base."""
+
+
+class Base:
+    """Representing the base of all other classes in out project."""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """class constructor"""
+        if id != None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
