5423aa038eeb98b225537ddde6ff56d6d8f1dbf2
iMGamal <mohamed.gamal126027@gmail.com>
2024-04-23 18:00:17 +0000

    1

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,101 +1,14 @@
-#!/usr/bin/python3
-"""
-Module that contains class Rectangle, inheritance of class Base
-"""
-from models.base import  Base
+#!/bin/usr/python3
+"""Module for class Rectangle and inheritance of class Base."""
+from models.base import Base
 
 
 class Rectangle(Base):
-    """class rectangle"""
-    def __init__(self, width, height, x=0, y=0, id=None):
-        """class constructor"""
-        super().__init__(id)
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
-
-    @property
-    def width(self):
-        """width of rectangle"""
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be > 0".format(name))
-        self.__width = value
-
-    @property
-    def height(self):
-        """height of rectangrl"""
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be > 0".format(name))
-        self.__height = value
-
-    @property
-    def x(self):
-        """x of rectangle"""
-        return self.__x
-
-    @x.setter
-    def x(self, value):
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be > 0".format(name))
-        self.__x = value
-
-    @property
-    def y(self):
-        """y of rectangle"""
-        return self.__y
-
-    @y.setter
-    def y(self, value):
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be > 0".format(name))
-        self.__y = value
-
-    def validate(self, name, value):
-        """Method for validating value"""
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be > 0".format(name))
-        elif name < 0:
-            raise ValueError("{} must be >= 0".format(name))
-
-    def area(self):
-        """Method to calculate rectangle area"""
-        return (self.width * self.height)
-
-    def display(self):
-        """Method to display rectangle"""
-        string = (self.width * '#' + '\n') * self.height
-        return(string)
-    def __str__(self, width, height, x, y, id=None):
-        s = '[Rectangle]' + ' ' + '(' + str(self.id) + ')' + ' ' + str(self.x) + '/' + str(self.y) + ' ' + '-' + ' ' + str(self.width) + '/' + str(self.height)
-        return (s)
-
-    def update(self, *args, **kwargs):
-        """Method to update rectangle values"""
-        args = (self.id, self.width, self.height, self.x, self.y)
-        kwargs = {}
-        for arg in args:
-            if args != None:
-                index = '[Rectangle]' + ' ' + '(' + str(args[0]) + ')' + ' ' + str(args[3]) + '/' + str(args[4]) + ' ' + '-' + ' ' + str(args[1]) + '/' + str(args[2])
-                return (index)
-            else:
-                index = '[Rectangle]' + ' ' + '(' + str(kwargs[4]) + ')' + ' ' + str(kwargs[2]) + '/' + str(kwargs[3]) + ' ' + '-' + ' ' + str(kwargs[0]) + '/' + str(kwargs[1])
-                return (index)
+    """class Rectangle"""
+    def update(self, *args):
+        """Method for passing arguments"""
+        self.id = args[0]
+        self.width = args[1]
+        self.height = args[2]
+        self.x = args[3]
+        self.y = args[4]
