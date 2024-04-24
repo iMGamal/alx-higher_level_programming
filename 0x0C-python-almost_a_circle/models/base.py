8f6c56e6dd1e6a85c12cf76615802c74eaa5d552
iMGamal <mohamed.gamal126027@gmail.com>
2024-04-24 20:34:53 +0000

    test1

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -11,6 +11,9 @@ class Base:
     """
     __nb_objects = 0
     def __init__(self, id=None):
+        """
+        class constructor.
+        """
         if id is not None:
             self.id = id
         else:
