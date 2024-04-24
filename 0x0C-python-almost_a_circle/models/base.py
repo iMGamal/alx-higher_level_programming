3ef00d59ef9dc7ca5301624ae919fe73e4f24196
iMGamal <mohamed.gamal126027@gmail.com>
2024-04-24 17:32:06 +0000

    test

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -7,15 +7,8 @@ Module for class Base.
 
 class Base:
     """
-    class representing the base of my project.
+    class representing the base of our project.
     """
-    def __init__(self, id=None):
-        """
-        class constructor.
-        """
-        __nb_objects = 0
-        if id is not None:
-            self.id = id
-        else:
-            __nb_objects += 1
-            self.id = __nb_objects
+    __nb_objects = 0
+
+
