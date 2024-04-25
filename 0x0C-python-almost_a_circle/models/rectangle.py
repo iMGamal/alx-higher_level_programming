6cbf2a2e9814861c8b57243e3a6f72d176766a69
iMGamal <mohamed.gamal126027@gmail.com>
2024-04-25 00:36:01 +0000

    all

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -21,32 +21,56 @@ class Rectangle(Base):
     
     @property
     def width(self):
+        """
+        Width getter.
+        """
         return self.__width
 
     @width.setter
     def width(self, a):
+        """
+        width setter.
+        """
         self.__width = a
 
     @property
     def height(self):
+        """
+        height getter.
+        """
         return self.__height
 
     @height.setter
     def height(self, b):
+        """
+        height setter.
+        """
         self.__height = b
 
     @property
     def x(self):
+        """
+        x getter.
+        """
         return self.__x
 
     @x.setter
     def x(self, c):
+        """
+        x setter.
+        """
         self.__x = c
 
     @property
     def y(self):
+        """
+        y getter.
+        """
         return self.__y
 
     @y.setter
     def y(self, d):
+        """
+        y setter.
+        """
         self.__y = d
