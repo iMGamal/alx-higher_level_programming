a88251875edba9a3ba78a5e27c38369f59ec3650
iMGamal <mohamed.gamal126027@gmail.com>
2024-04-26 17:39:33 +0000

    Testing

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -95,7 +95,7 @@ class Rectangle(Base):
         for i in range(self.y):
             print()
         for i in range(self.height):
-            print((" " * self.x) + ('#' * self.width) + '\n')
+            print((" " * self.x) + ('#' * self.width))
 
     def __str__(self):
         """
