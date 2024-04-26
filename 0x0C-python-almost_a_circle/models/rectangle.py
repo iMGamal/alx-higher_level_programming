76e44a19f16318c685c71334ef50e5c4c0e3dd94
iMGamal <mohamed.gamal126027@gmail.com>
2024-04-26 23:10:16 +0000

    Testing

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -102,3 +102,11 @@ class Rectangle(Base):
         Method that returns string representation of an object.
         """
         return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
+
+    def update(self, *args):
+        self.id = args[0]
+        self.width = args[1]
+        self.height = args[2]
+        self.x = args[3]
+        self.y = args[4]
+        print(Rectangle.__str__(self))
