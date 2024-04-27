f7573407749e704ab6c4ce924e786d765fcb10cf
iMGamal <mohamed.gamal126027@gmail.com>
2024-04-27 01:03:49 +0000

    Testing

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -107,4 +107,23 @@ class Rectangle(Base):
         """
         Method for updating our class.
         """
-        args = (self.id, self.width, self.height, self.x, self.y)
+        if len(args) == 1:
+            self.id = args[0]
+        if len(args) == 2:
+            self.id = args[0]
+            self.width = args[1]
+        if len(args) == 3:
+            self.id = args[0]
+            self.width = args[1]
+            self.height = args[2]
+        if len(args) == 4:
+            self.id = args[0]
+            self.width = args[1]
+            self.height = args[2]
+            self.x = args[3]
+        if len(args) == 5:
+            self.id = args[0]
+            self.width = args[1]
+            self.height = args[2]
+            self.x = args[3]
+            self.y = args[4]
