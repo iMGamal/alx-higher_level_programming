39649e2976d1e1af6839b73a3975c399c338e90e
iMGamal <mohamed.gamal126027@gmail.com>
2024-06-18 23:56:05 +0000

    tested

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
+// Prints a message depending on number of passed arguements
+
+
 const { argv } = require('node:process');
-if (argv.length <= 2)
-	{
-		console.log('No argument');
-	}
-if (argv.length === 3)
-	{
-		console.log('Argument found');
-	}
-if (argv.length > 3)
-	{
-		console.log('Arguments found');
-	}
+if (argv.length <= 2) {
+  console.log('No argument');
+} else if (argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
