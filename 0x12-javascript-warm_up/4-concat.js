36ccbfc6496de9db243269663210dcfa51ffbf79
iMGamal <mohamed.gamal126027@gmail.com>
2024-06-19 07:46:50 +0000

    Tested

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -2,10 +2,9 @@
 const { argv } = require('node:process');
 const arg1 = process.argv[2];
 const arg2 = process.argv[3];
-const arg3 = process.argv[4];
-if (arg2 && arg3) {
-  console.log(argv[3] + ' ' + 'is' + ' ' + argv[4]);
-} else if (arg2) {
-  console.log(argv[3] + ' ' + 'is' + ' ' + 'undefined');
+if (arg1 && arg2) {
+  console.log(argv[2] + ' ' + 'is' + ' ' + argv[3]);
 } else if (arg1) {
+  console.log(argv[2] + ' ' + 'is' + ' ' + 'undefined');
+} else if (!arg1 && !arg2) {
   console.log('undefined' + ' ' + 'is' + ' ' + 'undefined');}
