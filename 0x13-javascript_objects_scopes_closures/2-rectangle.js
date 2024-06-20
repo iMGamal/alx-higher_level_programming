8609e500817deb7207479f592e14d951c5203c15
iMGamal <mohamed.gamal126027@gmail.com>
2024-06-20 01:29:46 +0000

    3

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,17 @@
+#!usr/bin/node
+class Rectangle {
+  constructor(w, h); {
+    this.width = w;
+    this.height = h;
+    this.empty = {};
+  }
+  const checkerw = parseInt(width);
+  const checkerh = parseInt(height);
+  if (isNaN(checkerw)) {
+    width = empty;
+  }
+  if (isNan(checkerh)) {
+    height = empty
+  }
+}
+module.exports = Rectangle;
