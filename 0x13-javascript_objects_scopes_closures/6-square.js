d6dd0bcdad77fdc7b65c92c34326d9108497b4da
iMGamal <mohamed.gamal126027@gmail.com>
2024-06-20 20:45:55 +0000

    08

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,12 +1,16 @@
 #!/usr/bin/node
 module.exports = class Square extends require('./5-square.js') {
   charPrint (c) {
-    const ctr = c || 'X';
     for (let i = 0; i < this.size; i++) {
       let x = '';
       for (let j = 0; j < this.size; j++) {
-	x += ctr;
+	if (c) {
+	x += c;
 	}
+	else {
+        x += 'X';
+	}
+      }
       console.log(x);
     }
    }
