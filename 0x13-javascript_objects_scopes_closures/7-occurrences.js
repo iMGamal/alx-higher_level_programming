25daffb57c40ffb96ec5eabc8e0c09f74f96cfa8
iMGamal <mohamed.gamal126027@gmail.com>
2024-06-20 22:39:45 +0000

    Tested

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,17 +1,10 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
+  let x = 0;
   for (let i = 0; i < list.length; i++) {
-    let x = searchElement;
-    let y = searchElement;
-    if (list[i] === x) {
-      x = 1;
-      for (let j = i + 1; j < list.length; j++) {
-        if (list[j] === y) {
-        x++;
-        }
-      }
-    console.log(x);
-    break;
+    if (list[i] === searchElement) {
+    x++;
     }
   }
+  return x;
 }
