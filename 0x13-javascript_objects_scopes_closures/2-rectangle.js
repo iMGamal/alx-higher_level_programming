4f71782da956885f306096f5a807b874dd9807f7
iMGamal <mohamed.gamal126027@gmail.com>
2024-06-20 13:33:18 +0000

    3

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -3,10 +3,9 @@ class Rectangle {
   constructor (w, h) {
     this.width = parseInt(w);
     this.height = parseInt(h);
-    if (this.width <= 0) {
+
+    if (this.width <= 0 || this.height <= 0) {
     this.width = undefined;
-    }
-    if (this.height <= 0) {
     this.height = undefined;
     }
   }
