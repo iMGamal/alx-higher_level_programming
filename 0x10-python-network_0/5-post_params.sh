02225597565a975b80c054dce6863ad4e3f02706
iMGamal <mohamed.gamal126027@gmail.com>
2024-07-09 22:50:10 +0000

    task 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Using curl to display body of response
-curl -sd "param1=email: test@gmail.com & param2=subject: I will always be here for PLD" -X POST "${1}"
+curl -sd "param1=email:test@gmail.com&param2=subject:I will always be here for PLD" -X POST "${1}"
