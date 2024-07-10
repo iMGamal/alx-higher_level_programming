bd73a84968617de2f98b96f41afebab43020bdd4
iMGamal <mohamed.gamal126027@gmail.com>
2024-07-10 01:27:52 +0000

    task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module for fetching data from url."""
+
+
+import urllib.request
+
+
+def fetch(url):
+    """Method for fetching data from url."""
+    try:
+        req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    with:
+        urllib.request.urlopen(req) as response:
+            content = response.read()
+            print("Body response:")
+            print(f"\t- type: {type(content)}")
+            print(f"\t- content: {content}")
+            print(f"\t- utf8 content: {content.decode()}")
