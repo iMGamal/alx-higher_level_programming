c8a0addc9d4c78a4b7668ff385de17ad86e19067
iMGamal <mohamed.gamal126027@gmail.com>
2024-07-10 02:07:09 +0000

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,12 +7,11 @@ def fetch(url):
     """Method for fetching data from url."""
     try:
         req = urllib.request.Request(url)
-        with:
-            urllib.request.urlopen(req) as response:
-                content = response.read()
-                print("Body response:")
-                print(f"\t- type: {type(content)}")
-                print(f"\t- content: {content}")
-                print(f"\t- utf8 content: {content.decode()}")
-if __name__ = "__main__":
+        with urllib.request.urlopen(req) as response:
+            content = response.read()
+            print("Body response:")
+            print(f"\t- type: {type(content)}")
+            print(f"\t- content: {content}")
+            print(f"\t- utf8 content: {content.decode()}")
+if __name__ == "__main__":
     fetch('https://alx-intranet.hbtn.io/status')
