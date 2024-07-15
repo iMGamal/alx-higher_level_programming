33fa92555b9448e0fbe5293a4dd8756f8cd4cfb1
iMGamal <mohamed.gamal126027@gmail.com>
2024-07-15 23:25:32 +0000

    adios

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
-"""
-Module to disassemble python code.
-"""
-from dis import disassemble
-
 def magic_calculation(a, b):
     return 98 + a ** b
-
-disassemble(magic_calculation.__code__)
