9a2c9d1039c134f2d64b85123165f7e98b280fb0
iMGamal <mohamed.gamal126027@gmail.com>
2024-07-15 19:11:14 +0000

    improved

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-from sys import write
-from sys import argv
+from sys import stderr, argv
 with open(argv[1], 'w') as file:
     file.write('and that piece of art is useful - Dora Korpar, 2015-10-19\n')
