82b481c7c82f9937987956d92b52b6801742c142
imgamal <mohamed.gamal126027@gmail.com>
2024-07-19 15:09:09 -0700

    task 17 v5

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,5 @@
 -- Command that converts database to UTF8
 USE hbtn_0c_0;
-ALTER first_table
-CONVERT TO CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_unicode_ci
+ALTER TABLE first_table,
+CONVERT TO CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci
