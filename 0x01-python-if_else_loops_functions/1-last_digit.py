126c61993e20f7f61a9ae31e07a83204adf0d21c
iMGamal <mohamed.gamal126027@gmail.com>
2024-07-28 11:47:07 +0000

    Task 1 v4

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,13 +2,12 @@
 import random
 number = random.randint(-10000, 10000)
 n = str(number)
-if n[0] == '-' and int(n[-1]) != 0:
-    print(f"Last digit of {number} is -{n[-1]} and is less than 6 and not 0")
-if n[0] != '-' and int(n[-1]) > 5:
-    print(f"Last digit of {number} is {n[-1]} and is greater than 5")
-if n[0] != '-' and int(n[-1]) < 6 and int(n[-1]) != 0:
-    print(f"Last digit of {number} is {n[-1]} and is less than 6 and not 0")
-if int(n[-1]) == 0:
-    print(f"Last digit of {number} is {n[-1]} and is 0")
-else:
-    print("TypeError")
+if (n):
+    if n[0] == '-' and int(n[-1]) != 0:
+        print(f"Last digit of {number} is -{n[-1]} and is less than 6 and not 0")
+    elif n[0] != '-' and int(n[-1]) > 5:
+        print(f"Last digit of {number} is {n[-1]} and is greater than 5")
+    elif n[0] != '-' and int(n[-1]) < 6 and int(n[-1]) != 0:
+        print(f"Last digit of {number} is {n[-1]} and is less than 6 and not 0")
+    elif int(n[-1]) == 0:
+        print(f"Last digit of {number} is {n[-1]} and is 0")
