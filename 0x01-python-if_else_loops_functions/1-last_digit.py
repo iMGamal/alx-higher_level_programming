71924097a7e958d31cf0479d15eba2ac1c6a561d
iMGamal <mohamed.gamal126027@gmail.com>
2024-07-31 16:42:56 -0700

    Handling numbers with abs() and %

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 import random
+
 number = random.randint(-10000, 10000)
-n = str(number)
-if (n):
-    if n[0] == '-' and int(n[-1]) != 0:
-        print(f"Last digit of {number} is -{n[-1]} and is less than 6 and not 0")
-    elif n[0] != '-' and int(n[-1]) > 5:
-        print(f"Last digit of {number} is {n[-1]} and is greater than 5")
-    elif n[0] != '-' and int(n[-1]) < 6 and int(n[-1]) != 0:
-        print(f"Last digit of {number} is {n[-1]} and is less than 6 and not 0")
-    elif int(n[-1]) == 0:
-        print(f"Last digit of {number} is {n[-1]} and is 0")
+n = abs(number) % 10
+
+if number < 0:
+    n = -n
+
+if n > 5:
+    print(f"Last digit of {number} is {last_digit} and is greater than 5")
+elif n < 6 and n != 0:
+    print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
+else:
+    print(f"Last digit of {number} is {last_digit} and is 0")
