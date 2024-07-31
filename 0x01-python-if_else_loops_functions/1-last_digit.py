45b39aba433954ac6c029c13fab51eb17277776b
iMGamal <mohamed.gamal126027@gmail.com>
2024-07-31 16:45:38 -0700

    Handling numbers with abs() and %

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,8 +8,8 @@ if number < 0:
     n = -n
 
 if n > 5:
-    print(f"Last digit of {number} is {last_digit} and is greater than 5")
+    print(f"Last digit of {number} is {n} and is greater than 5")
 elif n < 6 and n != 0:
-    print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
+    print(f"Last digit of {number} is {n} and is less than 6 and not 0")
 else:
-    print(f"Last digit of {number} is {last_digit} and is 0")
+    print(f"Last digit of {number} is {n} and is 0")
