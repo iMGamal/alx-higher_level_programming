26911d757bbb2e2fcb2e67889065c2df7c90393d
iMGamal <mohamed.gamal126027@gmail.com>
2024-08-07 19:02:29 -0700

    Task 4

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	$('#DIV#toggle_header').on('click', function() {
+		if ($('#header').attr('class') === 'red') {
+			$('#header').attr('class', 'green');
+			} else {
+				$('#header').attr('class', 'red');
+			}
+	});
+});
+
