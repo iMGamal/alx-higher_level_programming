b0918807f2146fde24872ce8fd37db3bc4755bf7
iMGamal <mohamed.gamal126027@gmail.com>
2024-08-09 23:19:59 +0000

    Task1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""There are no modules for now."""
+
+
+class Square:
+    """Updated first class in OOP."""
+    def __init__(self, size):
+        """Class constructor."""
+        self.__size = size
