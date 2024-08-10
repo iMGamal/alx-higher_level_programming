e72f377dc566435099f2428eba8df924705c91e2
iMGamal <mohamed.gamal126027@gmail.com>
2024-08-10 08:09:46 +0000

    Task2 v2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,7 +6,8 @@ class Square:
     """Updated first class in OOP."""
     def __init__(self, size=0):
         """Class constructor."""
-        if type(size) != int:
+        self.__size = size
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
