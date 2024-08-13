577923cb9396b8750b063396956123a3c9865af7
iMGamal <mohamed.gamal126027@gmail.com>
2024-08-13 02:57:06 +0000

    Test2

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def islower(c):
-    if c.islower() == True:
+    if c.islower() is True:
         return c
+    elif c == "":
+        raise TypeError("string can't be empty")
     else:
         pass
