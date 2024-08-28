d1fa8dc44d426c7067493c7769967ddbcaadd18c
iMGamal <mohamed.gamal126027@gmail.com>
2024-08-28 14:07:34 +0000

    Tasks

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
-
+    result = ""
+    for i in str:
+        if ord(i) in range(ord('a'), ord('z') + 1):
+            arg = ord(i)
+            result += chr(arg - 32)
+        else:
+            result += i
+    print(result)
