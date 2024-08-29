0b359f4148b00a0f20205a97fbd886453dec61c9
iMGamal <mohamed.gamal126027@gmail.com>
2024-08-29 16:28:38 +0000

    Slowly

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for letter in reversed(range(97, 123)):
+    if letter % 2 != 0:
+        letter = chr(letter).upper()
+    else:
+        letter = chr(letter)
+    print(letter, end="")
---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    x = str[:n]
+    y = str[n + 1:]
+    print(x + y)
