0a02d4938ecb36028f93577a64cdcf0886287b62
iMGaml <mohamed.gamal126027@gmail.com>
2024-08-30 17:50:59 +0300

    Test

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,4 +2,4 @@
 from add_0 import add
 result = add(1, 2)
 if "__name__" == "__main__":
-    print("1 " + "+ " + "2 " + "= " + str(result))
+    print("1 " + "+ " + "2 " + "= " + "{:s}".format(result))
