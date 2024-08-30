d677d0b5b4698f332b1847a62b78233948830d6f
iMGaml <mohamed.gamal126027@gmail.com>
2024-08-30 22:13:26 +0300

    revamp

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,9 +5,11 @@ from sys import argv
 operators = ['+', '-', '*', '/']
 if __name__ == "__main__":
     if (len(argv) - 1) != 3:
-        print("Usage: ./100-my_calaculator.py <a> <operator> <b>\n1")
+        print("./100-my_calaculator.py <a> <operator> <b>")
+        exit(1)
     elif (len(argv) - 1) == 3 and argv[2] not in operators:
-        print("Unknown operator. Available operators: +, -, * and /\n1")
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
     elif (len(argv) - 1) == 3 and argv[2] in operators:
         if argv[2] == '+':
             result = add(int(argv[1]), int(argv[3]))
@@ -15,25 +17,21 @@ if __name__ == "__main__":
             y = argv[2]
             z = argv[3]
             print(str(x) + " " + str(y) + " " + str(z) + " = " + str(result))
-            print(0)
         elif argv[2] == '-':
             result = sub(int(argv[1]), int(argv[3]))
             x = argv[1]
             y = argv[2]
             z = argv[3]
             print(str(x) + " " + str(y) + " " + str(z) + " = " + str(result))
-            print(0)
         elif argv[2] == '*':
             result = mul(int(argv[1]), int(argv[3]))
             x = argv[1]
             y = argv[2]
             z = argv[3]
             print(str(x) + " " + str(y) + " " + str(z) + " = " + str(result))
-            print(0)
         elif argv[2] == '/':
-            result = div(int(argv[1]), int(argv[2]))
+            result = div(int(argv[1]), int(argv[3]))
             x = argv[1]
             y = argv[2]
             z = argv[3]
             print(str(x) + " " + str(y) + " " + str(z) + " = " + str(result))
-            print(0)
