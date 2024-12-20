e5ae0d477ae4d02ecd0f0a7bbf169ea27cf01bf6
iMGamal <mohamed.gamal126027@gmail.com>
2024-12-20 09:29:42 +0000

    Task 9

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 if (isNaN(process.argv[2]) === false) {
-  for (i = 0; i < process.argv[2]; i++) {
+  for (let i = 0; i < process.argv[2]; i++) {
     console.log('C is fun');
   }
 }
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 if (isNaN(process.argv[2]) === false) {
   square = "";
-  for (i = 0; i < process.argv[2]; i++) {
+  for (let i = 0; i < process.argv[2]; i++) {
     square += "X"
   }
-  for (j = 0; j < process.argv[2]; j++) {
+  for (let j = 0; j < process.argv[2]; j++) {
     console.log(square);
   }
 }
