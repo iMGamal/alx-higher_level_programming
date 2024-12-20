e98c7dce0a027ec4aeee00f6a478c0dc2417af26
iMGamal <mohamed.gamal126027@gmail.com>
2024-12-20 12:02:39 +0000

    Task 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const factArg = process.argv[2];
+let factOutput = 1;
+
+function factorialCalc (factArg)
+{
+  if (factArg >= 1 && isNaN(factArg) === false)
+  {
+    factOutput *= factArg;
+    return factorialCalc(factArg - 1);
+  }
+}
+
+factorialCalc(process.argv[2]);
+console.log(factOutput);
